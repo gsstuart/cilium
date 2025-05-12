ce211dd1cfea1e0a1f8eb8def4f7e9adc1ebe94f
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-05-12 16:54:16 +0200

    bpf: unconditionally define cilium_lb{4,6}_reverse_sk
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- bpf/bpf_sock.c
+++ bpf/bpf_sock.c
@@ -126,7 +126,6 @@ bool sock_proto_enabled(__u8 proto)
 	}
 }
 
-#ifdef ENABLE_IPV4
 struct {
 	__uint(type, BPF_MAP_TYPE_LRU_HASH);
 	__type(key, struct ipv4_revnat_tuple);
@@ -136,6 +135,7 @@ struct {
 	__uint(map_flags, LRU_MEM_FLAVOR);
 } cilium_lb4_reverse_sk __section_maps_btf;
 
+#ifdef ENABLE_IPV4
 static __always_inline int sock4_update_revnat(struct bpf_sock_addr *ctx,
 					       const struct lb4_backend *backend,
 					       const struct lb4_key *orig_key,
@@ -614,8 +614,6 @@ int cil_sock4_getpeername(struct bpf_sock_addr *ctx)
 
 #endif /* ENABLE_IPV4 */
 
-#if defined(ENABLE_IPV6) || defined(ENABLE_IPV4)
-#ifdef ENABLE_IPV6
 struct {
 	__uint(type, BPF_MAP_TYPE_LRU_HASH);
 	__type(key, struct ipv6_revnat_tuple);
@@ -625,6 +623,8 @@ struct {
 	__uint(map_flags, LRU_MEM_FLAVOR);
 } cilium_lb6_reverse_sk __section_maps_btf;
 
+#if defined(ENABLE_IPV6) || defined(ENABLE_IPV4)
+#ifdef ENABLE_IPV6
 static __always_inline int sock6_update_revnat(struct bpf_sock_addr *ctx,
 					       const struct lb6_backend *backend,
 					       const struct lb6_key *orig_key,
